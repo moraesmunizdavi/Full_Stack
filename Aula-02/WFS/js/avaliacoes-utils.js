cf7fe32cde0d5f9c0a497edc679f3146a0949e49
Davi Moraes Muniz <moraesmunizdavi@gmail.com>
2026-09-14 11:08:16 -0300

    Aula 02
    CSS: Flexbox, Grid e Mobile-First

---
+++ Aula-02/WFS/js/avaliacoes-utils.js
@@ -0,0 +1,32 @@
+function calcularMedia(avaliacoes) {
+  if(!avaliacoes || avaliacoes.length === 0){
+    return 0
+  }
+  const soma = avaliacoes.reduce(function(total,avaliacao){
+return total + avaliacao.nota;
+  },0)
+  return Math.round((soma / avaliacoes.length)*10)/10
+
+}
+
+function filtrarPorLugar(avaliacoes, lugarId) {
+ return avaliacoes.filter(function (avaliacao){
+  return avaliacao.lugarId === lugarId
+})
+}
+ 
+function filtrarPorNotaMinima(avaliacoes, notaMinima) {
+  return avaliacoes.filter(function(avaliacao){
+    return avaliacao.nota >= notaMinima
+})
+}
+
+function ordenarPorNota(avaliacoes, ordem) {
+  ordem = ordem || "desc"
+  const copia = avaliacoes.slice()
+  copia.sort(function(a,b){
+    return ordem === "asc" ? a.nota - b.nota : b.nota - a.nota
+  })
+  return copia
+}
+
